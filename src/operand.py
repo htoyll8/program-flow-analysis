03b22b9de2745a7ef6f3f8e66258cd6f878d1ba6
Tyler Holloway <tylerhollowaycs@gmail.com>
2025-01-24 16:12:40 -0500

    Operand class.

--- src/operand.py
+++ src/operand.py
@@ -1,2 +1,31 @@
-class Operand:
+from abc import ABC
+
+
+class Operand(ABC):
     pass
+
+
+class Int(Operand):
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+
+class Str(Operand):
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+
+class Bool(Operand):
+    def __init__(self, value: bool) -> None:
+        self.value = value
+
+
+class Var(Operand):
+    def __init__(self, name: str, value: Operand) -> None:
+        self.name = name
+        self.value = value
+
+
+class Label(Operand):
+    def __init__(self, value: str) -> None:
+        self.value = value
